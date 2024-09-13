f391b1171b0a1e920d3229bb0ec71824bee0287d
TranHoan <hoantph33008@fpt.edu.vn>
2024-09-13 18:44:53 +0700

    upload danhmuc

---
+++ resources/views/admins/blocks/header.blade.php
@@ -0,0 +1,18 @@
+{{-- @section("title")
+{{$title}}
+@endsection --}}
+<div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Quản lý admin</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="http://127.0.0.1:8000/sanphams">Home</a></li>
+            <li class="breadcrumb-item active">Dashboard v1</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admins\DanhMucController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::resource('danhmucs', DanhMucController::class);
 });
