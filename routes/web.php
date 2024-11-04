a9d95b5d3142c552382b492e68bb639a3516c87d
TranHoan <hoantph33008@fpt.edu.vn>
2024-11-04 11:21:07 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,27 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\admins\PtttController;
-use App\Http\Controllers\admins\ChucVuController;
-use App\Http\Controllers\admins\DanhMucController;
-use App\Http\Controllers\admins\SanPhamController;
-use App\Http\Controllers\admins\TaiKhoanController;
+
 use App\Http\Controllers\admins\DonHangController;
-use App\Http\Controllers\admins\BinhLuanController;
+use App\Http\Controllers\admins\PtttController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admins\DanhMucController;
+use App\Http\Controllers\admins\SanPhamController;
+use App\Http\Controllers\admins\ChucVuController;
+use App\Http\Controllers\clients\HomeController;
+use App\Http\Controllers\admins\TaiKhoanController;
+use App\Http\Controllers\Admins\AdminController;
+
 use App\Http\Controllers\AuthController;
 use App\Http\Middleware\CheckRoleAdminMiddleware;
+use GuzzleHttp\Middleware;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\clients\SanPhamController as ClientSanPham;
+use App\Http\Controllers\clients\GioHangController;
+use App\Http\Controllers\clients\DonHangController as  ClientDonHang;
+use App\Http\Controllers\clients\BinhLuanController as clientBinhLuan;
+use App\Http\Controllers\admins\BinhLuanController;
+use App\Http\Controllers\clients\TaiKhoanController as clientTaiKhoan;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -40,3 +41,8 @@ Route::get('register', [AuthController::class, 'showFormRegister']);
 Route::post('register', [AuthController::class, 'register'])->name('register');
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 //admin
+Route::get("/",function(){
+    return redirect()->route("client.index");
+});
+
+Route::get('/adminhome', [AdminController::class, 'index'])->name('adminhome');
