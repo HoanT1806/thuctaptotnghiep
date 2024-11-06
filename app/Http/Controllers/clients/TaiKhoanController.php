030de329b0e1eee8a36bdc83236c96c405e32c45
TranHoan <hoantph33008@fpt.edu.vn>
2024-11-06 13:21:48 +0700

    taikhoan

--- app/Http/Controllers/clients/TaiKhoanController.php
+++ app/Http/Controllers/clients/TaiKhoanController.php
@@ -43,5 +43,32 @@ class TaiKhoanController extends Controller
         
         return view("clients.taikhoans.dasboard", $data);
     }
-    
+    public function chitiet(){
+        if(Auth::check()){
+            if (session()->exists('cart')) {
+                $this->data["giohangs"] = session()->get('cart', []);
+            } else {
+                if (Auth::check()) {
+                    // Nếu người dùng đã đăng nhập, lấy giỏ hàng từ cơ sở dữ liệu và lưu vào session
+                    $giohang = GioHang::query()
+                        ->with("sanphams")
+                        ->where('nguoi_dung_id', Auth::id())
+                        ->get();
+                    session()->put('cart', $giohang);
+                    $this->data["giohangs"] = $giohang;
+                } else {
+                    // Nếu người dùng chưa đăng nhập, tạo giỏ hàng trống
+                    session()->put('cart', []);
+                    $this->data["giohangs"] = [];
+                
+                }
+            }
+            $title = "Tài khoản";
+            $data = $this->data;
+            $data["title"] = $title;
+            $data["taikhoan"] = Auth::user();
+            return view("clients.taikhoans.taikhoan", $data);
+        }
+        
+    }
 }
